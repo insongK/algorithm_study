0494d4e59784cd166d89138cde6dfb056635602f
parking-been <ysbeen00@gmail.com>
2026-01-29 21:22:41 +0900

    feat:p2504

---
+++ src/main/java/study/a01_ds/d01/p2504/Main_박다빈.java
@@ -0,0 +1,33 @@
+package main.java.study.a01_ds.d01.p2504;
+import java.io.*;
+import java.util.*;
+public class Main_박다빈 {
+    static Stack<Integer> stack = new Stack<>();
+    static Map<Character, Integer> map = Map.of('(',-1,
+            ')',-2,
+            '[',-3,
+            ']',-4);
+    static Map<Integer, Integer> pp = Map.of(-1,-2,
+            -2,-1,
+            -3,-4,
+            -4,-3);
+    public static void main(String [] args){
+        //( : -1, ) : -2, [ : -3, ] : -4
+
+        Scanner sc = new Scanner(System.in);
+        String w = sc.next();
+
+        int latest = 0;
+
+        for (int i=0;i<w.length();i++){
+            char c = w.charAt(i);
+            int mC = map.get(c);
+
+
+        }
+    }
+
+
+
+
+}
